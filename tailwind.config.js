3a787a16925b226d3e000f96757d96f90eac5272
mhaikalkal <isalhaikal12@gmail.com>
2022-06-09 13:36:38 +0700

    learning about position, fixed, relative, absolute, etc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  // 2 asteriks, artinya semua folder,
+  // 1 asterik, artinya semua file
+  // .{hmtl, js} yang berekstensi html dan js
   content: ["./public/**/*.{html, js}"],
   theme: {
     extend: {},
